1ffa9888a1884ee2c87e71094310c7dcea86b7d1
Kevin Pham <phamk2@classwork.engr.oregonstate.edu>
2025-11-04 15:28:14 -0800

    added starter code for project

---
+++ app.js
@@ -0,0 +1,63 @@
+// ########################################
+// ########## SETUP
+
+// Express
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+//dotENV
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+// Database
+const db = require('./database/db-connector');
+
+// Handlebars
+const { engine } = require('express-handlebars'); // Import express-handlebars engine
+app.engine('.hbs', engine({ extname: '.hbs' })); // Create instance of handlebars
+app.set('view engine', '.hbs'); // Use handlebars engine for *.hbs files.
+
+// ########################################
+// ########## ROUTE HANDLERS
+
+// READ ROUTES
+app.get('/', async function (req, res) {
+    try {
+        res.render('home'); // Render the home.hbs file
+    } catch (error) {
+        console.error('Error rendering page:', error);
+        // Send a generic error message to the browser
+        res.status(500).send('An error occurred while rendering the page.');
+    }
+});
+
+app.get('/products', async function (req, res) {
+    try {
+        const [products] = await db.query('SELECT * FROM Products;');
+        res.render('products', { products: products });
+    }
+    catch (error) {
+        console.error('Error executing query:', error);
+        // Send a generic error message to the browser
+        res.status(500).send(
+            'An error occurred while executing the database query.'
+        );
+    }
+});
+
+
+
+// ########################################
+// ########## LISTENER
+
+app.listen(PORT, function () {
+    console.log(
+        'Express started on http://localhost:' +
+            PORT +
+            '; press Ctrl-C to terminate.'
+    );
+});
